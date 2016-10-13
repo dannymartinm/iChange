6c648d942c85feece4bedbd1c7e4a07891d29933
Daniela Martin <daniela@esporaestudio.com>
2016-10-13 23:18:51 +0200

    Se modificó el exchangeDAO. ExecuteExchange. Falta terminar

--- src/main/java/com/proyecto/DAO/ExchangeDAO.java
+++ src/main/java/com/proyecto/DAO/ExchangeDAO.java
@@ -2,6 +2,7 @@ package com.proyecto.DAO;
 
 import com.proyecto.domain.Exchange;
 import com.proyecto.domain.User;
+import com.proyecto.domain.Article;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.stereotype.Repository;
 
@@ -19,13 +20,50 @@ public class ExchangeDAO {
         this.jdbcOperations = jdbcOperations;
     }
     //Solo he de hacer intercambio...,de lo demás se encargará el controlador
-    public  void executeExchange(User u1, int idA1,  User u2, int idA2 ){
+    public  void executeExchange(User u1, Article idA1,  User u2, Article idA2 ){
+
+        int new_quantity1 = getQuantity(idA1.getIdArticle());
+        int new_quantity2 = getQuantity(idA2.getIdArticle());
+
 
         String sql = "UPDATE article SET owner = ? WHERE idArticle= ?";
-        jdbcOperations.update(sql, u1.getIdUser(), idA2);
-        jdbcOperations.update(sql, u2.getIdUser(), idA1);
+
+        if (new_quantity1 == 1 && new_quantity2==1){
+            jdbcOperations.update(sql, u1.getIdUser(), idA2.getIdArticle());
+            jdbcOperations.update(sql, u2.getIdUser(), idA1.getIdArticle());
+        }
+        else {
+
+            if(new_quantity1 == 1 && new_quantity2>1){
+                //crear un nuevo articulo al 1 con quantity 1 y restar al 2
+
+
+            }
+            else if(new_quantity2 == 1 && new_quantity1>1){
+                //crear un nuevo articulo al 2 con quantity 1 y restar al 1
+
+            }
+            else {
+                jdbcOperations.update("UPDATE article SET quantity = ? WHERE idArticle = ?", new_quantity1 - 1, idA1.getIdArticle());
+                jdbcOperations.update("UPDATE article SET quantity = ? WHERE idArticle = ?", new_quantity2 - 1, idA2.getIdArticle());
+
+                jdbcOperations.update(sql, u1.getIdUser(), idA2.getIdArticle());
+                jdbcOperations.update(sql, u2.getIdUser(), idA1.getIdArticle());
+
+            }
+        }
+
+
+
         //restar cantidad, si tiene + de 1
         //debería de poner al exchange que el isDone = true;
+
+    }
+
+    private int getQuantity(int idArticle){
+        String sql = "SELECT quantity FROM article where idArticle = ?";
+        return jdbcOperations.queryForObject(sql, new Object[]{idArticle}, Integer.class);
+
     }
 
     public void evaluateExchange(double value, User user, Exchange exchange){
